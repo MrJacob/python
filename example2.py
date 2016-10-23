e9d8c7dbbd3db953ed5ab26d38601dce2074e019
Mr.Jacob <j_elias3@yahoo.com.mx>
2016-10-23 12:28:59 -0600

    example2.py was created

---
+++ example2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+print('\n' * 100)
+
+list =[9,8,6,2,7,9,0,4]
+
+#order list
+print(sorted(list))
+
+
+#order list reverse 
+
+print(sorted(list, reverse=True))
+
+
+#make the order permanently 
+
+list.sort()
+print(list)
+
+letter = ['Za', 'Fg', 'Az', 'Ij']
+
+
+print(sorted(letter))
+
+#permanently change
+
+#letter.sort()
